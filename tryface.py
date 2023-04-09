2200380817bc87c7b3f35f8eefb699f1cb605ce3
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-04-10 03:04:27 +0700

    adding some code for meeting 1

--- tryface.py
+++ tryface.py
@@ -3,17 +3,16 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture("vid1.mp4")
 while True:
     ret, frame = cap.read()
+    # tambahkan  code di bawah ini
     gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     faces = face_cascade.detectMultiScale(gray_image, 1.1, 5)
-
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+        cv2.putText(frame, 'Person', (x, y),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 150, 0), 2)
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
 
